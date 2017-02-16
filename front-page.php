df25b5063518f7fb962a8a5408ae33df713c9d2c
Ethan Choi <ethanchoi.812@gmail.com>
2017-02-16 20:19:04 +0800

    Updated css

--- front-page.php
+++ front-page.php
@@ -21,7 +21,7 @@
 					<p><?php echo get_theme_mod('showcase_text', 'My Awesome App was made with plain awesomeness Javascript'); ?></p>
 					<h5 class="tags javascript-tag"><a href=""><?php echo get_theme_mod('showcase_tag', 'Javascript'); ?></a></h5>
 				</div>
-				<h3 class="read-more"><a href="portfolio.html">See More!</a></h3>
+				<h3 class="read-more"><a href="iwebsg.com/portfolio">See More!</a></h3>
 			</div>
 		</section>
 	</section>
